cd393c7934058d5c5e5b738f8a6d7003aed825e8
balbot <balbot@users.noreply.github.com>
2015-05-02 13:11:56 -0500

    First final commit

---
+++ scheme_ide/courses/marks_course.py
@@ -0,0 +1,18 @@
+course = ['Mark\'s Scheme Course',  ('Lesson 1', ('Section 1', ('Type 4 into the shell.', '<value-test>', 4)),
+                                             ('Section 2', ('Type (+ 1 2) into the shell.', '<value-test>', 3))
+                                  ),
+ 
+                                  ('Lesson 2', ('Section 1', ('Write a function, add1, that adds 1 to an \nargument.',
+                                                             'add1', 
+                                                              lambda add1: add1(0) == 1 and add1(10) == 11 and add1(14) == 15
+                                                            )
+                                              ),
+
+                                              ('Section 2', ('Write a function, fib, to compute the nth \nfibbonacci number.',
+                                                            'fib',
+                                                             lambda fib: fib(0) == 1 and fib(9) == 55 and fib(12) == 233
+                                                            ) 
+                                              )
+                                  )
+                                                                
+         ]        
